4b47cd78e37283881d742bbc3618b1d558fa21e0
Andrew Drozdov <adrozdov@ebay.com>
2017-08-31 13:59:05 -0400

    Fix test.

--- test.py
+++ test.py
@@ -120,8 +120,8 @@ class TestLatexTreesSR(unittest.TestCase):
         expected = [
             ((0.,0.), (-3,-1)),
             ((-2.25,-0.75), (-1.5,-1)),
-            ((1.5,-0.5), (0,-1)),
             ((2.25,-0.75), (1.5,-1)),
+            ((1.5,-0.5), (0,-1)),
             ((0.,0.), (3,-1)),
             ]
         actual = latex_trees_sr(tokens, width, height)
