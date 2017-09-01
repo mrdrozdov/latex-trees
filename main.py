3d54ae4cf589195cad1f80b34330dc758b0c2b60
Andrew Drozdov <adrozdov@ebay.com>
2017-09-01 12:29:14 -0400

    Remove previous behavior.

--- main.py
+++ main.py
@@ -7,64 +7,8 @@ import gflags
 FLAGS = gflags.FLAGS
 
 
-EDGE = 'x'
-LEFT = 'l'
-RIGHT = 'r'
-
 SHIFT = 0
 REDUCE = 1
-
-
-def sign(x):
-    return -1 if x < 0 else 1
-
-
-def latex_trees(tokens, width, height, precision=2):
-
-    N = len(tokens)
-
-    xincrement = width/(N-1)/2
-    yincrement = height/(N-1)
-
-    leftx = -width/2
-    rightx = 0.0
-    lefty = height
-    righty = 0.0
-
-    # When left, use leftx + xincrement. Then increment leftx/rightx
-    # by xincrement.
-    # When right, use rightx + xincrement. Then increment leftx/rightx
-    # by xincrement.
-
-    lines = []
-
-    for i, tkn in enumerate(tokens):
-        if i == 0:
-            assert tkn == EDGE
-            start = (0.0, 0.0)
-        if i == N - 1:
-            assert tkn == EDGE
-            start = (0.0, 0.0)
-        if tkn == LEFT:
-            start = (leftx + xincrement, lefty - i * yincrement)
-            leftx += xincrement
-            rightx += xincrement
-        if tkn == RIGHT:
-            start = (rightx + xincrement, righty + i * yincrement)
-            leftx += xincrement
-            rightx += xincrement
-            leftx += xincrement
-            lefty += yincrement
-        previous = tkn
-
-        end = (-width/2 + 2 * i * xincrement, height)  # always the same
-
-        start = tuple(map(lambda x: round(x, precision), start))
-        end   = tuple(map(lambda x: round(x, precision), end))
-
-        line = (start, end)
-        lines.append(line)
-    return lines
 
 
 def latex_trees_sr(transitions, width, height, precision=2, verbose=False):
@@ -222,20 +166,15 @@ def latex_trees_sr(transitions, width, height, precision=2, verbose=False):
 
 if __name__ == '__main__':
 
-    gflags.DEFINE_string("input", "x,l,r,r,x", "input")
+    gflags.DEFINE_string("input", "00101", "input")
     gflags.DEFINE_float("width", 6.0, "width")
     gflags.DEFINE_float("height", -1.0, "height")
     gflags.DEFINE_integer("precision", 2, "precision")
-    gflags.DEFINE_enum("style", "simple", ["simple", "shift-reduce"], "style")
 
     FLAGS(sys.argv)
     print("Flags: " + json.dumps(FLAGS.flag_values_dict(), sort_keys=True, indent=4))
 
-    if FLAGS.style == "simple":
-        lines = latex_trees(FLAGS.input.split(','), FLAGS.width, FLAGS.height, FLAGS.precision)
-    elif FLAGS.style == "shift-reduce":
-        lines = latex_trees_sr(list(map(int, FLAGS.input)), FLAGS.width, FLAGS.height, FLAGS.precision)
+    lines = latex_trees_sr(list(map(int, FLAGS.input)), FLAGS.width, FLAGS.height, FLAGS.precision)
 
-    print(lines)
     for line in lines:
         print("\draw [line width=0.25mm] {} -- {};".format(line[0], line[1]))
